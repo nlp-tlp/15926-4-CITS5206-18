bb4d4dff3d148610f5533466599f9fa6586fe75d
Manishv005 <166606288+Manishv005@users.noreply.github.com>
2024-09-29 19:02:47 +0800

    Update networkx_plot.py

--- src/networkx_plot.py
+++ src/networkx_plot.py
@@ -128,23 +128,28 @@ def display_networkx_plot(data, search_term, parent_limit, children_limit):
 
     # JavaScript for handling node clicks in NetworkX Plot
     components.html(f"""
-        <div id="networkx-container">
+        <div id="networkx-container" style="height: 600px; padding: 0; margin: 0; 
+    border: 5px solid transparent;  /* Create space for the border */
+    border-image: linear-gradient(90deg, #FFD700, #002855) 1;  /* Gradient matching NLP TLP logo */
+    box-sizing: border-box; /* Ensure padding and border are included in width and height calculations */
+    box-shadow: 0 0 10px rgba(0,0,0,0.05); overflow: hidden;">
             {graph_html}  <!-- Embed PyVis graph -->
         </div>
         
         <div style="display: flex; justify-content: center; align-items: center; margin-top: 20px;">
-            <button id="fullscreen-btn" onclick="toggleFullscreen()" style="
-                padding: 10px 20px; 
-                font-size: 16px; 
-                background: linear-gradient(90deg, #87CEEB 0%, #FFD700 100%);  /* Gradient from light blue to yellow */
-                color: white;  /* Text color */
-                border: 3px solid #002855;  /* Navy blue border */
-                border-radius: 5px; 
-                cursor: pointer; 
-                box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);">
-                Go Fullscreen
-            </button>
+                <button id="fullscreen-btn" onclick="toggleFullscreen()" style="
+                    padding: 10px 20px; 
+                    font-size: 16px; 
+                    background: linear-gradient(90deg, #FFD700, #002855);  /* Gradient from light blue to yellow */
+                    color: white;  /* Text color */
+                    border: 3px solid #002855;  /* Navy blue border */
+                    border-radius: 5px; 
+                    cursor: pointer; 
+                    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);">
+                    Go Fullscreen
+                </button>
         </div>
+
 
         <script>
             function toggleFullscreen() {{
