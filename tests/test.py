99a72323452a6d844357ae86ca43de732920b889
PriyaRaj-92 <priya92raj90@gmail.com>
2024-10-05 20:22:17 +0800

    Update test.py
    Implemented testing for selecting levels of superclasses and subclasses.

--- tests/test.py
+++ tests/test.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 # Path to ChromeDriver -need to be changed to the local chromedriver path
 driver_path = "C:/Users/shanm/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe"
@@ -62,6 +63,52 @@ def test_search_functionality(setup_browser):
             option.click()
             break  # Exit the loop after clicking
 
-    time.sleep(120)
+   
+
+    # Wait for the Number of levels of SUPERCLASSES to be visible
+    number_input = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.ID, "number_input_1"))
+    )
+
+    # Clear the input field by sending 'BACKSPACE' multiple times
+    number_input.click()
+    number_input.send_keys(Keys.BACKSPACE * 3)  # Clear existing value by deleting
+
+    # Send the new value '4' using the keyboard
+    number_input.send_keys("4")
+
+    # Press 'ENTER' to ensure the value is submitted
+    number_input.send_keys(Keys.TAB)
+
+    # Wait for a short moment to ensure the change is processed
+    time.sleep(1)
+
+    # Assert the value has been updated to '4'
+    updated_value = number_input.get_attribute("value")
+    assert updated_value == "4", f"Number input did not update correctly! Current value: {updated_value}"
+
+    ## Wait for the Number of levels of SUBCLASSES to be visible
+    number_input = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.ID, "number_input_2"))
+    )
+
+    # Clear the input field by sending 'BACKSPACE' multiple times
+    number_input.click()
+    number_input.send_keys(Keys.BACKSPACE * 3)  # Clear existing value by deleting
+
+    # Send the new value '4' using the keyboard
+    number_input.send_keys("4")
+
+    # Press 'ENTER' to ensure the value is submitted
+    number_input.send_keys(Keys.TAB)
+
+    # Wait for a short moment to ensure the change is processed
+    time.sleep(1)
+
+    # Assert the value has been updated to '4'
+    updated_value = number_input.get_attribute("value")
+    assert updated_value == "4", f"Number input did not update correctly! Current value: {updated_value}"
+
+    time.sleep(60)
         
     
